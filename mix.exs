9e71877892bb52353c2462cbb44f1b9f4d196ad3
Erin Boeger <sullymustycode@gmail.com>
2022-09-10 18:26:57 +0900

    bump version number

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ActiveMemory.MixProject do
   @github "https://github.com/SullysMustyRuby/active_memory"
   @license "MIT"
   @name "ActiveMemory"
-  @version "0.1.4"
+  @version "0.2.0"
 
   def project do
     [
